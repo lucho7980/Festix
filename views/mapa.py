885bd5772dc74743470c7629ed8c0c367dfa27be
Naim M. C. Medina <naimmchmm@hotmail.com>
2023-08-01 16:36:37 -0300

    view/mapa.py

--- views/mapa.py
+++ views/mapa.py
@@ -3,6 +3,7 @@ from tkintermapview import TkinterMapView
 import tkintermapview as mapa
 import customtkinter as ctk
 
+
 ctk.set_appearance_mode("dark") 
 #COLORES
 blanco = "#eee"
@@ -15,6 +16,7 @@ class VistaMapa(ctk.CTk):
         super().__init__(master)
         self.master = master
         self.controlador = controlador
+        self.ubicaciones = []
         
         #RAIZ
         self.title("FESTIX")
@@ -27,9 +29,9 @@ class VistaMapa(ctk.CTk):
         self.titulo = ctk.CTkLabel(self,text="MAPA FESTIX",font=("Open Sans",30),text_color=blanco).pack(pady=30,padx=95)
         
         #WIDGET MAPA
-        self.mapa = mapa.TkinterMapView(self,width=600, height=400)
+        self.mapa = mapa.TkinterMapView(self,width=600, height=400,corner_radius=10)
         self.mapa.pack(pady=10,padx=10)
-        self.mapa.set_position(48.8584, 2.2945) #PARIS
+        self.mapa.set_address("Salta,Salta,Argentina",marker=True) #PARIS
         
         #BOTONES
         ubicaciones = ctk.CTkButton(
@@ -47,6 +49,23 @@ class VistaMapa(ctk.CTk):
            # command=self.controlador.regresar_ubicaciones
             ).pack(pady=5,padx=10)
         
+        
+        def Añadir_marcador_de_evento(cords):
+            """
+            Recibe las coordenadas proporcionadas por add_left_click_map_command y las agrega al mapa.
+            Además crea una tupla con los datos de la ubicación agregada en una lista.
+            """
+            lista = []
+            nombre = input("Nombre de la ubicación: ")
+            click = self.mapa.set_marker(cords[0], cords[1], text=nombre)
+            direccion = (cords[0],cords[1])
+            lista.append((nombre,direccion,[cords[0], cords[1]]))
+            mensaje = tk.messagebox.showinfo("Eventos",f"'{nombre}' se agregó con éxito")
+           
+            return lista
+
+        self.ubicaciones = self.mapa.add_left_click_map_command(Añadir_marcador_de_evento)
+
 if __name__ == "__main__":
     app = VistaMapa()
     app.mainloop()
